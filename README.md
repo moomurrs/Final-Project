3373e8fb8b6f87ba95748c43b8535cb4f17d1ff8
nahon1 <60366732+nahon1@users.noreply.github.com>
2020-04-28 22:56:49 -0400

    background
    added

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Game Jam
 
 Original design by Casey
 Character sprite from gameart2d.com
+Added game background
---
+++ Scenes/Sprite.gd
@@ -0,0 +1,9 @@
+extends Sprite
+
+
+var viewportWidth = get_viewport().size.x
+var viewportHeight = get_viewport().size.y
+
+var scale = viewportWidth / $Sprite.texture.get_size().x
+
+
